fc606727a3c05aa435d646de0e271e376994f826
Wallace_Lee <gn01675522@gmail.com>
2023-12-17 21:22:43 +0800

    refactor: organize route to route group

---
+++ components/AlbumCard/AlbumReview.component.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import "./AlbumCard.style.scss";
+
+const AlbumCard = () => {
+  return (
+    <div className="album-card">
+      <div className="album-card__title">
+        <span>icon</span>
+        <h3>quidem molestiae enim</h3>
+      </div>
+      <div className="album-card__img">temp</div>
+    </div>
+  );
+};
+
+export default AlbumCard;
