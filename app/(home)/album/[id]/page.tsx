379cd603b5067a47c3639b13bb57ecb8e1bdb283
Wallace_Lee <gn01675522@gmail.com>
2023-12-18 19:46:48 +0800

    refactor: delete album-detail's route and migrate it's page.js and page.scss to /album/[id] style: change main page & Tabbar & ImgCard & AlbumReview's style

--- app/(home)/album/[id]/page.tsx
+++ app/(home)/album/[id]/page.tsx
@@ -1,11 +1,27 @@
+import ImgCard from "@/components/ImgCard/ImgCard.component";
+
 import "./page.scss";
 
-const AlbumPage = () => {
+const AlbumDetailPage = () => {
   return (
-    <div>
-      <div>Album detail page</div>
-    </div>
+    <main className="album-detail">
+      <h1 className="album-detail__title">quidem molestiae enim</h1>
+      <div className="album-detail__list">
+        <ImgCard />
+        <ImgCard />
+        <ImgCard />
+        <ImgCard />
+        <ImgCard />
+        <ImgCard />
+        <ImgCard />
+        <ImgCard />
+        <ImgCard />
+        <ImgCard />
+        <ImgCard />
+        <ImgCard />
+      </div>
+    </main>
   );
 };
 
-export default AlbumPage;
+export default AlbumDetailPage;
